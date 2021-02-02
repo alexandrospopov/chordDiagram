f89a572d3c05e172f30be5d68b2d7ac6278b2d04
Alexandros Popov <alexandros.ppv@gmail.com>
2021-02-02 14:56:41 +0100

    loading ok.

--- dataProcessing.py
+++ dataProcessing.py
@@ -1,8 +1,17 @@
-def writeChordData( labelJson, matrix ):
+import json
 
-  print( labelJson, matrix )
+def writeChordData( labelJsonFileName, matrixFileName ):
+
+  print( labelJsonFileName, matrixFileName )
+
+  with open( labelJsonFileName, "r" ) as f:
+    labels = json.load( f )
+
+  with open( matrixFileName, "r" ) as f:
+    matrix = f.readlines()
 
 
+  print( labels, matrix )
 
 if __name__=="__main__":
 
