b49a9846a3299f081d7357877c8a1b58a5b481db
Alexandros Popov <alexandros.ppv@gmail.com>
2021-02-05 12:15:16 +0100

    first upadate ok.

--- mainChord.js
+++ mainChord.js
@@ -11,6 +11,7 @@ const innerRadius = outerRadius - 20
 
 const ribbon = d3.ribbon()
                  .radius(innerRadius - 4)
+                 
 const arc = d3.arc()
               .innerRadius(innerRadius)
               .outerRadius(outerRadius)
@@ -25,6 +26,10 @@ const svg = mainChord.attr('width', width)
                      .attr("viewBox", [-width / 2, -height / 2, width, height])
                      .attr("font-size", 10)
                      .attr("font-family", "sans-serif");
+
+const canvas = svg.append("g")
+                  .attr("fill-opacity", 0.67)
+                  .attr("id", "canvas")
 
 color = d3.scaleOrdinal( d3.schemeBrBG[11] )
 
@@ -47,59 +52,64 @@ function drawChord( chordData,  labelData)
 {
   var chords = chord( chordData );
   
-  const group = svg.selectAll("g")
-                   .data(chords.groups)
-                   .enter().append("g");
+  // var group = svg.selectAll( "g" )
+  //                .data( chords.groups )
+  //                .enter().append("g");
 
-  const arcLabel = group.append("path")
-                        .attr("class", "arcLabel")
-                        .attr("d", arc)
-                        .attr("id", (d,i) => "arcLabel_" + i ) 
-                        .attr("fill", (d,i) => color( i ))
-                        .attr("stroke", (d,i) => d3.rgb(color( i )).darker())
+  // var arcLabel = group.append("path")
+  //                     .attr("class", "arcLabel")
+  //                     .attr("d", arc)
+  //                     .attr("id", (d,i) => "arcLabel_" + i ) 
+  //                     .attr("fill", (d,i) => color( i ))
+  //                     .attr("stroke", (d,i) => d3.rgb(color( i )).darker())
 
-  const ribbons = svg.append("g")
-                       .attr("fill-opacity", 0.67)
-                     .selectAll("path")
-                     .data(chords)
-                     .enter().append("path")
-                       .attr("class", "ribbons")
-                       .attr("d", ribbon)
-                       .attr("originalValue", d =>  d.source.value )
-                       .style("fill", d =>  "url(#" + getGradID(d) + ")" )
-      
+  var ribbons = d3.select("#canvas").selectAll("path")
+                                    .data( chords, getGradID )
+
+  console.log( chords)
+  ribbons.exit().remove()
+                      
+  ribbons.enter().append( "path" )
+                 .attr( "class", "ribbons" )
+                 .attr( "d", ribbon )
+                //  .style("fill", d =>  "url(#" + getGradID(d) + ")" )
+
+  d3.select("#canvas").selectAll("path").attr('d', ribbon )
+                          
+
+
   function getGradID(d){ 
     return "linkGrad-" + d.source.index + "-" + d.target.index; }
 
-  //Create the gradients definitions for each chord
-  const grads = svg.append("defs").selectAll("linearGradient")
-    .data(chords)
-    .enter().append("linearGradient")
-    .attr("id", getGradID)
-    .attr("gradientUnits", "userSpaceOnUse")
-    .attr("x1", d => innerRadius * Math.cos((d.source.endAngle-d.source.startAngle)/2 + d.source.startAngle - Math.PI/2) )
-    .attr("y1", d => innerRadius * Math.sin((d.source.endAngle-d.source.startAngle)/2 + d.source.startAngle - Math.PI/2) )
-    .attr("x2", d => innerRadius * Math.cos((d.target.endAngle-d.target.startAngle)/2 + d.target.startAngle - Math.PI/2) )
-    .attr("y2", d => innerRadius * Math.sin((d.target.endAngle-d.target.startAngle)/2 + d.target.startAngle - Math.PI/2) )
+  // //Create the gradients definitions for each chord
+  // const grads = svg.append("defs").selectAll("linearGradient")
+  //   .data(chords)
+  //   .enter().append("linearGradient")
+  //   .attr("id", getGradID)
+  //   .attr("gradientUnits", "userSpaceOnUse")
+  //   .attr("x1", d => innerRadius * Math.cos((d.source.endAngle-d.source.startAngle)/2 + d.source.startAngle - Math.PI/2) )
+  //   .attr("y1", d => innerRadius * Math.sin((d.source.endAngle-d.source.startAngle)/2 + d.source.startAngle - Math.PI/2) )
+  //   .attr("x2", d => innerRadius * Math.cos((d.target.endAngle-d.target.startAngle)/2 + d.target.startAngle - Math.PI/2) )
+  //   .attr("y2", d => innerRadius * Math.sin((d.target.endAngle-d.target.startAngle)/2 + d.target.startAngle - Math.PI/2) )
 
-  //Set the starting color (at 0%)
-  grads.append("stop")
-    .attr("offset", "0%")
-    .attr("stop-color", d => color( d.source.index ) );
+  // //Set the starting color (at 0%)
+  // grads.append("stop")
+  //   .attr("offset", "0%")
+  //   .attr("stop-color", d => color( d.source.index ) );
 
-  //Set the ending color (at 100%)
-  grads.append("stop")
-    .attr("offset", "100%")
-    .attr("stop-color", d => color( d.target.index ) );
+  // //Set the ending color (at 100%)
+  // grads.append("stop")
+  //   .attr("offset", "100%")
+  //   .attr("stop-color", d => color( d.target.index ) );
 
-  group.append("text" )
-    .attr("x", 2)
-    .attr("dy", -3)
-    .append("textPath")
-      .attr("xlink:href", (d, i) =>  "#arcLabel_" + i )
-      .text( (d, i) =>  labelData[ i ] ) 
-      .style("fill", "#35978f")
-      .style('font-weight', 'bold');
+  // group.append("text" )
+  //   .attr("x", 2)
+  //   .attr("dy", -3)
+  //   .append("textPath")
+  //     .attr("xlink:href", (d, i) =>  "#arcLabel_" + i )
+  //     .text( (d, i) =>  labelData[ i ] ) 
+  //     .style("fill", "#35978f")
+  //     .style('font-weight', 'bold');
 
 }
 
@@ -153,27 +163,39 @@ function initializeAreaChoice( labelData )
 // }
 
 
-function dataWoSelfConnection(){
+function dataWoSelfConnection( show){
   Promise.all([ d3.json( "data.json" ), ]).then(function( file ) 
 {
   chordData = file[0].chordData
   labelData = file[0].labelData
 
 
-  chordDataModified = chordData
-  for (let i = 0; i < chordData.length; i++) {
-      chordDataModified[ i ][ i ] = 0
+  if (show)
+  {
+    drawChord( chordData, labelData ) 
   }
-
-
-  drawChord( chordDataModified, labelData )
+  else
+  {
+    for (let i = 0; i < chordData.length; i++) 
+    {
+        chordData[ i ][ i ] = 0
+    }
+    drawChord( chordData, labelData )
+  }
 })
 }
 
 
 d3.select("#cb_selfLink")
-  .on("click", dataWoSelfConnection );
-
-
+  .on("click", function(){
+    this.checked ? dataWoSelfConnection( 1 ) : dataWoSelfConnection( 0 ) 
+  } )
 
 initializeChord()
+
+
+// d3.select("#cb_cities")
+//   .on("click", function() {
+//       let currentVisibility = this.checked ? "visible" : "hidden";
+//       d3.selectAll('.marker').attr('visibility', currentVisibility)
+// });
