e25c424645407be7689f8747e5120c90113af79a
Alexandros Popov <alexandros.ppv@gmail.com>
2021-02-10 10:29:15 +0100

    corrected init.

--- public/js/launchViz.js
+++ public/js/launchViz.js
@@ -5,7 +5,10 @@ function initializeViz(){
   {
     chordData = file[0].chordData
     labelData = file[0].labelData
-  
+
+    document.getElementById("cb_selfLink").checked = true;
+    document.getElementById("rb_lineaire").checked = true;
+
     drawChord( chordData, labelData )
     initializeAreaChoice( labelData )
     initializeBrush( chordData )
