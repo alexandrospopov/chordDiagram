011fe99c4389478d5017b7a2fe881da292e82559
Alexandros Popov <alexandros.ppv@gmail.com>
2021-06-07 14:56:28 +0200

    first output.

--- public/python/bundlesDataProcessing.py
+++ public/python/bundlesDataProcessing.py
@@ -7,66 +7,6 @@ def loadData( bundlesFileName ):
 
   return bundlesJson
 
-def processData( labels, matrix ): 
-
-  for key in labels:
-    labels[ key ] = labels[ key ].split( " " )
-    labels[ key ] = [ int( num ) for num in labels[ key ] ]
-
-  matrix = matrix[0].split( " " )
-
-  return ( labels, matrix )
-
-def createChordData( labels ):
-  
-  placeHolder = [ 0 for i in range( len( labels ) ) ]
-  chordData = [ list( placeHolder ) for i in range( len( labels ) ) ]
-
-  return chordData
-    
-def createPositionKey( labels ):
-
-  orderedKeys = labels.keys()
-  orderedKeys.sort(key=lambda v: v.upper())
-
-  positionKey = {}
-  for iKey, key in enumerate( orderedKeys ):
-    positionKey[ key ] = iKey
-
-  return positionKey, orderedKeys
-
-def getRank( positionKey, labels, value ):
-
-  for key in labels:
-    
-    if value in labels[ key ]:
-
-      return positionKey[ key ]
-
-  else:
-
-    print( "%i is not a known label value." % value )  
-    return -1
-
-
-def classifyMatrix( matrix ):
-
-  source = []
-  dest = []
-  quant = []
-  
-  for iNum, num in enumerate( matrix ):
-    if num == '':
-      pass
-    elif iNum % 3 == 0 :
-      source.append( int( num ) )
-    elif iNum % 3 == 1 :
-      dest.append( int( num ) )
-    elif iNum % 3 == 2 :
-      quant.append( int( num ) )
-
-  return source, dest, quant 
-
 def writeChordData( bundlesFileName, clustersJson, fibersJson ):
 
   print( "\nProcessing data in %s . \n" % bundlesFileName )
@@ -76,33 +16,34 @@ def writeChordData( bundlesFileName, clustersJson, fibersJson ):
   fibreCounts = bundlesJson[ "curve3d_counts" ]
   labels = bundlesJson[ "labels" ]
 
+  nameLabels = []
+  for label in labels:
+    subLabels = label.split( "_" )
+    for subLabel in subLabels[ : 2 ]:
+      if subLabel not in nameLabels:
+        nameLabels.append( subLabel )
 
-  # labels, matrix = processData( labels, matrix )
-  
-  # chordData = createChordData( labels )
-  # positionKey, orderedKeys = createPositionKey( labels )
+  placeHolder = [ 0 for i in range( len( nameLabels ) ) ]
+  labelCounter = [ list( placeHolder ) for i in range( len( nameLabels ) ) ]
 
-  # source, dest, quant = classifyMatrix( matrix )
+  for label in labels:
+    [ source,dest ] = label.split( "_" )[ : 2 ]
+    ( sourceIndex, destIndex ) = ( 
+                          nameLabels.index( source ), nameLabels.index( dest ) )
 
-  # for s,d,q in zip( source, dest, quant ):
+    labelCounter[ sourceIndex ][ destIndex ] += 1
+    labelCounter[ destIndex ][ sourceIndex ] += 1
 
-  #   rankS = getRank( positionKey, labels, s )
-  #   rankD = getRank( positionKey, labels, d )
 
-  #   if rankS > 0 and rankD > 0:
-  #     chordData[ rankS ][ rankD ] += q 
-  #     chordData[ rankD ][ rankS ] += q 
- 
+  dataClusters = {
+    "chordData" : labelCounter,
+    "labelData" : nameLabels
+  }
 
-  # data = {
-  #   "chordData" : chordData,
-  #   "labelData" : orderedKeys
-  # }
+  with open( clustersJson, 'w' ) as json_file:
+    json.dump( dataClusters, json_file )
 
-  # with open( outputFileName, 'w' ) as json_file:
-  #   json.dump( data, json_file )
-
-  # print( "\nWrote : %s \n" % outputFileName )
+  print( "\nWrote : %s \n" % clustersJson )
 
 
 if __name__=="__main__":
