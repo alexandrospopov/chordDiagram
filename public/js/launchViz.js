da23e1516675b6128dfa568e5cfdcf3b85f1065b
Alexandros Popov <alexandros.ppv@gmail.com>
2021-06-09 19:27:34 +0200

    defined fileToLoad.

--- public/js/launchViz.js
+++ public/js/launchViz.js
@@ -14,7 +14,7 @@ var openFile = function(event) {
 
 function initializeViz(){
 
-  Promise.all([ d3.json( "dataClusters.json" ), ]).then(function( file ) 
+  Promise.all([ d3.json( fileToLoad ), ]).then(function( file ) 
   {
     chordData = file[0].chordData
     labelData = file[0].labelData
